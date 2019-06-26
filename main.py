7aa3791edec5bb620a1acc3a49933fcfadb8287b
mgallimore88 <mgallimore88@gmail.com>
2019-06-26 22:59:51 +0100

    integrating a different sound package - soundcard- which enables output audio to speaker object through a numpy array. Limitation of the sounddevice package used on the master branch is that the play method requires the python script to pause for the duration of played audio, hence a 2 second audio output would pause the planet simulation for 2 seconds and not have desired effect.

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from sin_gen import SinGen
+import soundcard as sc
 
 y = SinGen(500)
 
-y.make_sin()
+# get a list of all speakers:
+speakers = sc.all_speakers()
+
+'''All of these functions return Speaker and Microphone objects,
+which can be used for playback and recording. All data passed in and out
+of these objects are frames × channels Numpy arrays.'''
--- sin_gen.py
+++ sin_gen.py
@@ -1,12 +1,13 @@
 import sounddevice as sd
 import numpy as np
 import math
+import soundcard as sc
 
 
 class SinGen:
     def __init__(self, frequency=500):
         self.frequency = frequency  # cycles per second
-        self.sample_frequency = 44100  # Hz
+        self.sample_frequency = 10  # Hz
         self.sampling_interval = 1 / self.sample_frequency  # secs
         self.length_secs = 1
         self. num_of_samples = round(self.sample_frequency * self.length_secs)
@@ -19,6 +20,11 @@ class SinGen:
             self.sample_array[n] = math.sin(2 * math.pi * self.frequency *
                                             self.time)
             self.time += self.sampling_interval
+        numpy_sin_array = np.array(self.sample_array)
+        print(numpy_sin_array)
         
-        sd.play(self.sample_array, self.sample_frequency)
-        sd.wait()
+        def sounddevice_play(self):
+            sd.play(self.sample_array, self.sample_frequency)
+            sd.wait()
+
+        def soundcard_play(self):
